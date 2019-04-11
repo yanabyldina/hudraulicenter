cc290e5daf9d0c6fe912c92797f0cb710fd3704f
Yana Byldina <yanabyldina@gmail.com>
2019-04-11 07:38:55 +0000

    Project Created

--- js/product-list.js
+++ js/product-list.js
@@ -16,22 +16,18 @@ class ProductList {
         products.forEach(product => {
             productListDomString +=
                 `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
-                  <div class="card product">
-                  <div class="card-img-top">
-            <div id="carousel">
-              <span id="buttonBackward"><button class="arrow">⇦</button></span>
-              <span>
-  <ul>
-     <li><img src="img/${product.image}"  alt="${product.title}"></li>
-    <li><img src="img/${product.image}"  alt="${product.title}"></li>
-    <li><img src="img/${product.image}"  alt="${product.title}"></li>   
-  </ul></span>
-              <span id="buttonForward"><button class="arrow">⇨</button></span>
-            </div>
-          </div>
+               <div class="card product">
+                    <img class="card-img-top" src="img/${product.image}" 
+                        alt="${product.title}">
                     <div class="card-body">
                       <h4 class="card-title">${product.title}</h4>
                       <p class="card-text">${product.description}</p>
+                      <button class="btn btn-info" data-toggle="modal"
+                        data-target="#productInfoModal" data-id="${product.id}">Info
+                      </button>
+                      <button class="btn btn-primary buy" data-id="${product.id}">
+                        ${product.price}UAH - Buy
+                      </button>
                     </div>
                   </div>
                 </div>`;

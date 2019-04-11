608bfc3f30680d383e13b74afd6f3f34763751d7
Yana Byldina <yanabyldina@gmail.com>
2019-04-11 07:34:25 +0000

    Project Created

--- js/product-list.js
+++ js/product-list.js
@@ -1,11 +1,11 @@
 class ProductList {
-    constructor (productsUrl, renderContainer) {
+    constructor(productsUrl, renderContainer, cart) {
+        this.cart = cart;
         fetch(productsUrl)
-            .then(result => result.json() )
+            .then(result => result.json())
             .then(products => {
                 this.products = products;
                 this.renderProducts(renderContainer, products);
-                
             })
     }
     getProductById(id) {
@@ -14,25 +14,28 @@ class ProductList {
     renderProducts(container, products) {
         let productListDomString = ''
         products.forEach(product => {
-            productListDomString += 
+            productListDomString +=
                 `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
                   <div class="card product">
-                    <img class="card-img-top" src="img/${product.image}" 
-                        alt="${product.title}">
+                  <div class="card-img-top">
+            <div id="carousel">
+              <span id="buttonBackward"><button class="arrow">⇦</button></span>
+              <span>
+  <ul>
+     <li><img src="img/${product.image}"  alt="${product.title}"></li>
+    <li><img src="img/${product.image}"  alt="${product.title}"></li>
+    <li><img src="img/${product.image}"  alt="${product.title}"></li>   
+  </ul></span>
+              <span id="buttonForward"><button class="arrow">⇨</button></span>
+            </div>
+          </div>
                     <div class="card-body">
                       <h4 class="card-title">${product.title}</h4>
                       <p class="card-text">${product.description}</p>
-                      <button class="btn btn-info" data-toggle="modal"
-                        data-target="#productInfoModal" data-id="${product.id}">Info
-                      </button>
-                      <button class="btn btn-primary buy" data-id="${product.id}">
-                        ${product.price}UAH - Buy
-                      </button>
                     </div>
                   </div>
                 </div>`;
         });
         container.html(productListDomString);
     }
- 
 }
